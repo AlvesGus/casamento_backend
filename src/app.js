47993dbdfd47c5b2adf4e83366792efb57c9676d
Gustavo Alves dos Santos <gustavo.tbt@hotmail.com.br>
2026-01-29 20:50:38 -0300

    fix: cors credentials habilited

--- src/app.js
+++ src/app.js
@@ -8,9 +8,12 @@ app.use(
   cors({
     origin: ['http://localhost:3000', 'https://seu-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   })
 )
+
+app.options('*', cors()) // preflight
 
 app.use(express.json())
 app.use('/api', routes)
