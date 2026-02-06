c1318ad47625b6cc3bf099fa90f7e219131fb038
Gustavo Alves dos Santos <gustavo.tbt@hotmail.com.br>
2026-02-06 01:04:09 -0300

    fix: new cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://cha-de-panelas-frontend.vercel.app/'
+      'https://cha-de-panelas-frontend.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
