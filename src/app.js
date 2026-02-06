378084833bb7b1e1bf436c017d08145ed720dbad
Gustavo Alves dos Santos <gustavo.tbt@hotmail.com.br>
2026-02-06 01:00:13 -0300

    feat: new cors for frontend

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://seu-frontend.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://cha-de-panelas-frontend.vercel.app/'
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
